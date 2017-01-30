c9b6b2df69f2cce307838d6f3cbf49699b33524d
Ali Paikan <ali.paikan@gmail.com>
2017-01-30 15:21:16 +0100

    simplifying example

--- port/main.cpp
+++ port/main.cpp
@@ -1,47 +1,42 @@
 #include <yarp/os/Network.h>
-#include <yarp/os/Port.h>
 #include <yarp/os/LogStream.h>
+#include <yarp/os/Port.h>
+#include <yarp/os/Bottle.h>
+#include <yarp/os/Time.h>
 
-#include <yarp/os/all.h>
+//#include <yarp/os/all.h>
 
 using namespace std;
 using namespace yarp::os;
 
 int main(int argc, const char **argv) {
-    
+
     // initialize yarp network
     Network yarp;
 
-    
-    Port inPort;
-    if (!inPort.open("/relay/in")) {
-        yError() << "cannot open the input port";
+    // open the outut port "/relay/out"
+    Port outPort;
+    if (!outPort.open("/relay/out")) {
+        yError() << "cannot open the output port";
         return -1;
     }
-
-    Port outPort;
-    // open the outut port "/relay/out"
-    // ...
 
     int counter = 0;
     while (true) {
         counter++;
 
-        yInfo()<<" waiting for input...";
-        Bottle input, output;
-
-        // read from input port 
-        // ...
-
-        // prepare the output data 
-        output.addInt(counter);
+        // prepare the output data
+        Bottle output;
+        //output.addInt(counter);
         // output.addString(...)
         // ...
 
         // write the output data
         yInfo()<< "writing data ...";
         //...
+
+        // wait for a second
+        Time::delay(1.0);
     }
     return 0;
 }
-
